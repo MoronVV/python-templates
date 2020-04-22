c9808a2966b202047f44c925b1a57677aa8b16af
Veselov Vladislav <vrveselo@mts.ru>
2020-04-22 10:18:06 +0300

    Fix async function

--- {{cookiecutter.project_slug}}/app/src/views.py
+++ {{cookiecutter.project_slug}}/app/src/views.py
@@ -7,6 +7,6 @@ routes = web.RouteTableDef()
 
 
 @routes.get('/')
-def index(request):
+async def index(request):
     logger.debug("I'm a teapot")
     return web.Response(text="I'm a teapot", status=418)
